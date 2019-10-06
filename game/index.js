e202f03187ff4ea31f1bc8015e32cb6bde288a6e
Victor Eyer <eyervictor@gmail.com>
2019-10-06 11:14:56 -0300

    plant Mines

--- game/index.js
+++ game/index.js
@@ -16,13 +16,24 @@ var a = {
         });
         this.template.forEach(function (element1, index1, array1) {
             array1.forEach(function (element2, index2, array2) {
-                a.template[index1][index2] = index1 +''+ index2;
+                a.template[index1][index2] = "";
             });
         });
     },
     plantMines(qnt){
-        this.mines = {
-
+        if (qnt >= Math.floor((this.sizex * this.sizey)/8)) {
+            return console.log("ERROR: too much 💣's");
+        }
+        let i = 0;
+        min = Math.ceil(0);
+        max = Math.floor(this.sizex - 1);
+        while (i < qnt){
+            line = Math.floor(Math.random() * (max - min + 1)) + min;
+            column = Math.floor(Math.random() * (max - min + 1)) + min;
+            if (!(a.template[line][column] === "x")){
+                a.template[line][column] = "x";
+                i++;
+            }
         }
     }
 };
